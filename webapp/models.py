a48b4484c458357e7a28a950b922816b3f92e9f1
SaeedAmini <saeedamini506750@gmail.com>
2024-05-20 12:47:49 +0430

    sand id from index to show_product  for show one product

--- webapp/models.py
+++ webapp/models.py
@@ -1,20 +1,20 @@
+# models.py
 import mysql.connector
-from flask import current_app
+from .mydb import database
 
 def get_db_connection():
-    config = current_app.config['DATABASE_CONFIG']
     connection = mysql.connector.connect(
-        host=config['host'],
-        user=config['user'],
-        password=config['password'],
-        database=config['database']
+        host=database['host'],
+        user=database['user'],
+        password=database['password'],
+        database=database['database']
     )
     return connection
 
-def get_products():
+def get_products_ending_with_a():
     connection = get_db_connection()
     cursor = connection.cursor(dictionary=True)
-    query ="""SELECT * FROM image INNER JOIN product
+    query = """SELECT * FROM image INNER JOIN product
 ON product.product_ID = image.product_ID
 WHERE image.image_name LIKE '%(1)%';"""
     cursor.execute(query)
@@ -22,3 +22,21 @@ WHERE image.image_name LIKE '%(1)%';"""
     cursor.close()
     connection.close()
     return products
+
+def get_product_by_id(product_id):
+    connection = get_db_connection()
+    cursor = connection.cursor(dictionary=True)
+    query = """
+    SELECT p.*, GROUP_CONCAT(i.image_name) as images
+    FROM product p
+    LEFT JOIN image i ON p.product_ID = i.product_ID
+    WHERE p.product_ID = %s AND i.image_active = 'yes'
+    GROUP BY p.product_ID;
+    """
+    cursor.execute(query, (product_id,))
+    product = cursor.fetchone()
+    cursor.close()
+    connection.close()
+    if product and 'images' in product:
+        product['images'] = product['images'].split(',')
+    return product
--- webapp/views.py
+++ webapp/views.py
@@ -1,10 +1,15 @@
 # views.py
-from flask import Blueprint, render_template
-from .models import get_products
+from flask import Blueprint, render_template, request
+from .models import get_products_ending_with_a, get_product_by_id
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def index():
-    products = get_products()
+    products = get_products_ending_with_a()
     return render_template("index.html", products=products)
+
+@views.route('/product/<int:product_id>')
+def show_product(product_id):
+    product = get_product_by_id(product_id)
+    return render_template("show_product.html",product=product)
